756397c0c7059532ee0ba054bc2db006487f876b
김민우 <0chickim@gmail.com>
2024-08-23 09:54:27 +0900

    chore: biome setup

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,10 @@ import "./index.css";
 import { enableMocking } from "./lib/msw/index.ts";
 
 enableMocking().then(() => {
+  // biome-ignore lint/style/noNonNullAssertion: <explanation>
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
-    </StrictMode>
+    </StrictMode>,
   );
 });
