abcc46c9344d8fd7db89a55fad2a33108f4054b9
김민우 <0chickim@gmail.com>
2024-08-24 11:31:54 +0900

    feat: lnb 레이아웃 수정

--- src/components/layouts/default-layout.tsx
+++ src/components/layouts/default-layout.tsx
@@ -6,7 +6,11 @@ export function DefaultLayout() {
     <div className="h-full grid grid-cols-1 md:grid-cols-[200px_1fr]">
       <LNB />
       <div className="h-full">
-        <header>여기가 헤더</header>
+        <header className="px-4 py-2 border-b">
+          <div className=" h-9">
+            <h1 className="text-lg font-semibold">IncerPay Admin</h1>
+          </div>
+        </header>
         <main>
           <Outlet />
         </main>
--- src/components/layouts/lnb.tsx
+++ src/components/layouts/lnb.tsx
@@ -5,32 +5,62 @@ import { NavItem } from "../ui/nav-item";
 
 export function LNB() {
   return (
-    <div className="h-full hidden md:block border-zinc-300 border">
-      <div className="border-b border-zinc-300 px-2 py-4">
+    <div className="h-full hidden border-r md:flex md:flex-col">
+      <div className="border-b px-2 py-2">
         <UserCombobox />
       </div>
 
-      <nav className="py-2 border-b border-zinc-300">
-        <ul className="space-y-2 px-2">
-          {CUSTOMER_NAV.map((navItem) => {
-            return (
-              <li key={navItem.label} className="space-y-1">
-                <NavLink to={navItem.path}>
-                  {({ isActive }) => {
-                    return (
-                      <NavItem
-                        IconComponent={navItem.IconComponent}
-                        isActive={isActive}
-                        label={navItem.label}
-                      />
-                    );
-                  }}
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <div className="flex-1 grid grid-cols-1">
+        <nav className="py-2 border-b">
+          <h3 className="px-2 mb-2 text-sm font-semibold text-muted-foreground">
+            Customer Menu
+          </h3>
+          <ul className="space-y-2 px-2">
+            {CUSTOMER_NAV.map((navItem) => {
+              return (
+                <li key={navItem.label} className="space-y-1">
+                  <NavLink to={navItem.path}>
+                    {({ isActive }) => {
+                      return (
+                        <NavItem
+                          IconComponent={navItem.IconComponent}
+                          isActive={isActive}
+                          label={navItem.label}
+                        />
+                      );
+                    }}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        <nav className="py-2 border-b">
+          <h3 className="px-2 mb-2 text-sm font-semibold text-muted-foreground">
+            System Menu
+          </h3>
+          <ul className="space-y-2 px-2">
+            {CUSTOMER_NAV.map((navItem) => {
+              return (
+                <li key={navItem.label} className="space-y-1">
+                  <NavLink to={navItem.path}>
+                    {({ isActive }) => {
+                      return (
+                        <NavItem
+                          IconComponent={navItem.IconComponent}
+                          isActive={isActive}
+                          label={navItem.label}
+                        />
+                      );
+                    }}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
